c39fab33fa5fb62a9ac00ca7377c3957dd430dc6
AnandhuAnilkumar <anandhuae03@gmail.com>
2024-12-12 07:26:43 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center h-screen justify-center bg-amber-100">
+      <h1 className="h1">
+        StoreIt - The only storage solution you need.
+      </h1>
+    </div>
+  );
+}
