0446f6478a89f4000f9d35b3213a025876ebd22b
AnandhuAnilkumar <anandhuae03@gmail.com>
2024-12-12 20:34:42 +0530

    OTP Modal

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,6 +18,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { createAccount } from "@/lib/actions/user.actions";
+import OTPModal from "./OTPModal";
 
 type FormType = "sign-in" | "sign-up";
 
@@ -150,6 +151,10 @@ const AuthForm = ({ type }: { type: FormType }) => {
         </form>
       </Form>
       {/*OTP verification*/}
+
+      {accountId && (
+        <OTPModal email={form.getValues("email")} accountId={accountId} />
+      )}
     </>
   );
 };
