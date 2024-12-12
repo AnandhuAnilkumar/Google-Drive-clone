b610c3e123c995afca37334d5369eb72c775bd34
AnandhuAnilkumar <anandhuae03@gmail.com>
2024-12-12 23:02:27 +0530

    Sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex items-center h-screen justify-center bg-amber-100">
+    <div className="flex items-center h-screen justify-center ">
       <h1 className="h1">
         StoreIt - The only storage solution you need.
       </h1>
