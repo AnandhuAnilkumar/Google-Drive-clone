6b6432f6307b580c780802538f9cd15c5bc9cca6
AnandhuAnilkumar <anandhuae03@gmail.com>
2024-12-14 06:36:03 +0530

    Mobile Navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import { Button } from './ui/button'
-import Image from 'next/image'
-import FileUploader from './FileUploader'
-import Search from './Search'
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import FileUploader from "./FileUploader";
+import Search from "./Search";
 
 const Header = () => {
-  return <header className='header'>
-  <Search /> 
+  return (
+    <header className="header">
+      <Search />
 
-  <div className='header-wrapper'>
-    <FileUploader /> 
+      <div className="header-wrapper">
+        <FileUploader />
 
-    <form>
-      <Button type='submit' className='sign-out-button'>
-          <Image src="/assets/icons/logout.svg" alt="logo" width={24} height={24} className='w-6' />
-      </Button>
-    </form>
-  </div>
-  </header>
-}
+        <form>
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="logo"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
